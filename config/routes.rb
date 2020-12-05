ecaa9bc14c3a3c9d1314db5d3910d5fad51dc99a
mona <monaragab122@gmail.com>
2020-12-05 00:54:16 -0800

    add bootstrap to the project

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/weather', to: 'weather#search'
     end
+    root to: 'city_weather#index'
+    get '/search', to: 'city_weather#search'
   end
 end
