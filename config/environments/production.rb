996e606a79c1684172fdab9238cd95295ab51926
mona <monaragab122@gmail.com>
2020-12-05 21:08:40 -0800

    change configuration of default url option in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'afternoon-sierra-33909.heroku.com' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'afternoon-sierra-33909.heroku.com',
+    user_name: 'user@website.com',
+    password: 'password',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
