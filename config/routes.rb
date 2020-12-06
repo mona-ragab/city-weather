e1c171bb170b6aac2905b47dc65d7fb8c344dd0a
mona <monaragab122@gmail.com>
2020-12-05 17:02:12 -0800

    add devise for authentication and change views files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    namespace :v1 do
-      get '/weather', to: 'weather#search'
-    end
-    root to: 'city_weather#index'
-    get '/search', to: 'city_weather#search'
+  devise_for :users
+  namespace :v1 do
+    get '/weather', to: 'weather#search'
   end
+  root to: 'city_weather#index'
+  get '/search', to: 'city_weather#search'
 end
